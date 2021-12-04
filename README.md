d1ea868170ed857ebad361b5e499d486d3d2a653
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2021-12-04 10:15:17 -0800

    Update version of SDK, version strings

--- README.md
+++ README.md
@@ -5,27 +5,19 @@ For more details about the Azure Embedded SDK for C please refer to the [oficial
 This library package contains the following samples.
 Please refer to their documentation for setup and execution instructions.
 
-[ESPRESSIF ESP-8266](examples/aziot_esp8266/readme.md)
+[ESPRESSIF ESP-8266](examples/Azure_IoT_Hub_ESP8266/readme.md)
 
-[ESPRESSIF ESP-32](examples/aziot_esp32/readme.md)
+[ESPRESSIF ESP-32](examples/Azure_IoT_Hub_ESP32/readme.md)
 
-[Realtek AmebaD](examples/aziot_realtek_amebaD/readme.md)
+[Realtek AmebaD](examples/Azure_IoT_Hub_RealtekAmebaD/readme.md)
 
 ## Contributing
 
-For reporting any issues or requesting support, please open an issue on [azure-sdk-for-c](https://github.com/azure/azure-sdk-for-c.git).
+For reporting any issues or requesting support, please open an issue on [azure-sdk-for-c](https://github.com/Azure/azure-sdk-for-c/issues/new/choose).
 
 This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/).
 For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
 contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
-
-## Trademarks
-
-This project may contain trademarks or logos for projects, products, or services. Authorized use of Microsoft 
-trademarks or logos is subject to and must follow 
-[Microsoft's Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
-Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.
-Any use of third-party trademarks or logos are subject to those third-party's policies.
 
 ### Reporting Security Issues and Security Bugs
 
--- tools/Update-Library.ps1
+++ tools/Update-Library.ps1
@@ -1,23 +1,41 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # SPDX-License-Identifier: MIT
 
+param(
+	$SdkVersion = $(throw "SdkVersion not provided"),
+	$NewLibraryVersion = $(throw "NewLibraryVersion not provided")
+)
+
 $SrcFolder = "..\src"
 $LibConfigFile = "..\library.properties"
 
-git clone https://github.com/Azure/azure-sdk-for-c sdkrepo
+Write-Host "Clonning azure-sdk-for-c repository."
+
+git clone -b $SdkVersion https://github.com/Azure/azure-sdk-for-c sdkrepo
+
+Write-Host "Flattening the azure-sdk-for-c file structure and updating src/."
 
 $Files = gci -Recurse -Include *.h, *.c .\sdkrepo\sdk  | ?{ $_.DirectoryName -INOTMATCH "tests|sample" -AND $_.Name -INOTMATCH "curl|win32|az_posix" }
 
-copy $Files $SrcFolder
+rm -Force -Exclude "azure_ca.h" $SrcFolder/*
+
+copy -Verbose $Files $SrcFolder
+
+Write-Host "Filtering out files not needed/supported on Arduino."
 
 Get-ChildItem -Recurse -Include *.c,*.h -Path $SrcFolder | %{
-	$(Get-Content -Raw $_ ) -replace "<azure`/(iot`/internal|core`/internal|iot|core)`/", "<" | out-file -Encoding ascii -Force -NoNewline $_
+	$(Get-Content -Raw $_ ) -replace "<azure`/(iot`/internal|core`/internal|iot|core|storage)`/", "<" | out-file -Encoding ascii -Force -NoNewline $_
 }
+
+Write-Host "Removing clone of azure-sdk-for-c."
 
 rm -Recurse -Force sdkrepo/
 
+Write-Host "Updating versions."
+
 # Update Arduino library version with SDK version.
-$SdkVersion = $(gc $SrcFolder\az_version.h | ?{ $_ -IMATCH "AZ_SDK_VERSION_STRING" }).Split()[2].Replace("`"", "")
+$(gc -Raw $LibConfigFile) -replace "version=[0-9]\.[0-9]\.[0-9][a-zA-Z0-9.-]*", "version=$NewLibraryVersion" | out-file -Encoding ascii -Force -NoNewline $LibConfigFile
 $(gc -Raw $LibConfigFile) -replace "\([0-9]\.[0-9]\.[0-9][^\)]*\)", "($SdkVersion)" | out-file -Encoding ascii -Force -NoNewline $LibConfigFile
+$(gc -raw $LibConfigFile) -replace "url=[a-zA-Z0-9\/:.-]+", "url=https://github.com/Azure/azure-sdk-for-c/tree/$SdkVersion" | out-file -Encoding ascii -Force -NoNewline $LibConfigFile
 
 Write-Host "You must manually update the library.properties with any new includes such as az_core.h, az_iot.h"
