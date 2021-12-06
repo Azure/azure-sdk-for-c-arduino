1450e34bd21e47e5a082db942c8cbba044b251a2
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2021-12-06 15:02:45 -0800

    Fix latest comments from PR review (typos, logging)

--- tools/Update-Library.ps1
+++ tools/Update-Library.ps1
@@ -9,20 +9,20 @@ param(
 $SrcFolder = "..\src"
 $LibConfigFile = "..\library.properties"
 
-Write-Host "Clonning azure-sdk-for-c repository."
+Write-Host "Cloning azure-sdk-for-c repository."
 
 git clone -b $SdkVersion https://github.com/Azure/azure-sdk-for-c sdkrepo
 
 Write-Host "Flattening the azure-sdk-for-c file structure and updating src/."
 
+# Filtering out files not needed/supported on Arduino.
 $Files = gci -Recurse -Include *.h, *.c .\sdkrepo\sdk  | ?{ $_.DirectoryName -INOTMATCH "tests|sample" -AND $_.Name -INOTMATCH "curl|win32|az_posix" }
 
 rm -Force -Exclude "azure_ca.h" $SrcFolder/*
 
 copy -Verbose $Files $SrcFolder
 
-Write-Host "Filtering out files not needed/supported on Arduino."
-
+# Fixing headers to work as a flat structure.
 Get-ChildItem -Recurse -Include *.c,*.h -Path $SrcFolder | %{
 	$(Get-Content -Raw $_ ) -replace "<azure`/(iot`/internal|core`/internal|iot|core|storage)`/", "<" | out-file -Encoding ascii -Force -NoNewline $_
 }
