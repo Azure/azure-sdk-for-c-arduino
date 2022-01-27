b277e1b27ca84001b842f547fd21681978d980cb
niswitze <niswitze@microsoft.com>
2022-01-26 21:22:30 -0600

    Initial commit with Platform IO sample for ESP8266. The goal of this new sample is to show how developers can use the Azure Embedded C SDK with Platform IO.

---
+++ examples/Azure_IoT_Hub_ESP8266/Platform IO/include/iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID ""
+#define IOT_CONFIG_WIFI_PASSWORD ""
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "[HubName].azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID ""
+#define IOT_CONFIG_DEVICE_KEY ""
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
