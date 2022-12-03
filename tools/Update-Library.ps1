af4654be9bfe3d489beaf7e56df9ec6146ecbf9b
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2022-12-02 17:33:13 -0800

    Add documentation to Update-Library.ps1

--- tools/Update-Library.ps1
+++ tools/Update-Library.ps1
@@ -1,6 +1,28 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # SPDX-License-Identifier: MIT
 
+<#
+.SYNOPSIS
+	Updates the Azure SDK for C code referenced by this Arduino library.
+.DESCRIPTION
+	Update-Library does:
+	- copy over the code from azure/azure-sdk-for-c into the flat file structure stored under .\src.
+	- Change the code from azure/azure-sdk-for-c (copied locally) to reference headers in the .\src directory.
+	- Update the version of this Arduino library.
+.PARAMETER SdkBranch
+	Commit-ish (e.g., branch or tag) from azure/azure-sdk-for-c to use during the local file update.
+.PARAMETER SdkVersion
+	String with the version information to update in this Arduino library library.properties/paragraph.
+.PARAMETER NewLibraryVersion
+	String with the new version of this Arduino library (updated in library.properties/version).
+.EXAMPLE
+	Update-Library -SdkBranch main -SdkVersion 1.3.2 -NewLibraryVersion 1.0.0
+.EXAMPLE
+	Update-Library -SdkBranch feature/adu -SdkVersion 1.3.2 -NewLibraryVersion 1.1.0-beta.1
+.EXAMPLE
+	Update-Library -SdkBranch 1.4.0 -SdkVersion 1.4.0 -NewLibraryVersion 1.1.0
+#>
+
 param(
   $SdkBranch = $(throw "SdkBranch not provided"),
   $SdkVersion = $(throw "SdkVersion not provided"),
